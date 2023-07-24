41558ffa8878d1c52461cdfd2865d78fd0aa2de8
Kaue Varela <kauevarella@hotmail.com>
2023-07-24 18:00:17 -0300

    Class position created

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import board.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position p = new Position(1,2);
+		System.out.println(p);
+		
+	}
+
+}
